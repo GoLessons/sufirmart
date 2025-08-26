5646e5dd253732b069266a5e66eaba55950e5630
Alexey Sklyarov <asklyarov@ubic.tech>
2025-08-26 14:12:29 +0300

    draft: api server generated

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,25 @@
+//go:generate go tool oapi-codegen -config ../../tools/oapi.yaml ../../specification.yaml
+
 package main
 
-func main() {}
+import (
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+	"sufirmart/internal/api"
+)
+
+func main() {
+	apiServer := api.Unimplemented{}
+
+	router := chi.NewMux()
+
+	mainHandler := api.HandlerFromMux(apiServer, router)
+
+	server := &http.Server{
+		Handler: mainHandler,
+		Addr:    "0.0.0.0:8080",
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
