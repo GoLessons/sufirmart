4b4f4a6599d523491cc2ed895e6cff6e4a2b8f7f
Alexey Sklyarov <sufir@mihailovka.info>
2025-09-04 17:11:54 +0300

    feat: working with transactions

--- internal/repository/account.go
+++ internal/repository/account.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"github.com/Masterminds/squirrel"
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"sufirmart/internal/domain"
 )
@@ -43,4 +44,46 @@ func (r *AccountRepository) GetBalance(ctx context.Context, userID domain.UserID
 	balance := domain.NewBalance(userID, accrued.Float64, withdrawn.Float64)
 
 	return &balance, nil
+}
+
+func (r *AccountRepository) RegisterTransaction(ctx context.Context, userID domain.UserID, orderNum string, accrual float64, comment string) (string, error) {
+	transactionID := uuid.NewString()
+	builder := squirrel.StatementBuilder.
+		PlaceholderFormat(squirrel.Dollar).
+		Insert(`"sufirmart"."transaction"`).
+		Columns("id", "user_id", "order_num", "accrual", "withdraw", "status", "comment").
+		Values(transactionID, userID.String(), orderNum, accrual, 0.0, int16(domain.TransactionStatusPlanned), comment)
+
+	_, err := builder.RunWith(r.db).ExecContext(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	return transactionID, nil
+}
+
+func (r *AccountRepository) CancelTransaction(ctx context.Context, transactionID string, reason string) error {
+	sb := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+	update := sb.Update(`"sufirmart"."transaction"`).
+		Set("status", int16(domain.TransactionStatusCanceled)).
+		Set("comment", reason).
+		Set("processed_at", squirrel.Expr("NOW()"))
+
+	builder := update.Where(squirrel.Eq{"id": transactionID})
+
+	_, err := builder.RunWith(r.db).ExecContext(ctx)
+	return err
+}
+
+func (r *AccountRepository) ApproveTransaction(ctx context.Context, transactionID string, accrual float64) error {
+	sb := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+	update := sb.Update(`"sufirmart"."transaction"`).
+		Set("status", int16(domain.TransactionStatusProcessed)).
+		Set("accrual", accrual).
+		Set("processed_at", squirrel.Expr("NOW()"))
+
+	builder := update.Where(squirrel.Eq{"id": transactionID})
+
+	_, err := builder.RunWith(r.db).ExecContext(ctx)
+	return err
 }
