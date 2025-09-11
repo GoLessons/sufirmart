1d7d84fb8218ed806891294b10a62e208ada08ff
Alexey Sklyarov <sufir@mihailovka.info>
2025-09-11 16:49:46 +0300

    fix: accrual http client

--- internal/accrual/contract.go
+++ internal/accrual/contract.go
@@ -45,3 +45,76 @@ func NewOrderInProcessing(orderNum string) *OrderInProcessing {
 func (e *OrderInProcessing) Error() string {
 	return fmt.Sprintf("order %s in processing", e.orderNum)
 }
+
+type BuildURLError struct {
+	err error
+}
+
+func NewBuildURLError(err error) *BuildURLError {
+	return &BuildURLError{err: err}
+}
+
+func (e *BuildURLError) Error() string {
+	return fmt.Sprintf("build endpoint: %v", e.err)
+}
+
+func (e *BuildURLError) Unwrap() error {
+	return e.err
+}
+
+type RequestError struct {
+	err error
+}
+
+func NewRequestError(err error) *RequestError {
+	return &RequestError{err: err}
+}
+
+func (e *RequestError) Error() string {
+	return fmt.Sprintf("request accrual: %v", e.err)
+}
+
+func (e *RequestError) Unwrap() error {
+	return e.err
+}
+
+type DecodeError struct {
+	err error
+}
+
+func NewDecodeError(err error) *DecodeError {
+	return &DecodeError{err: err}
+}
+
+func (e *DecodeError) Error() string {
+	return fmt.Sprintf("decode accrual response: %v", e.err)
+}
+
+func (e *DecodeError) Unwrap() error {
+	return e.err
+}
+
+type AccrualServiceError struct {
+	StatusCode int
+	Status     string
+}
+
+func NewAccrualServiceError(code int, status string) *AccrualServiceError {
+	return &AccrualServiceError{StatusCode: code, Status: status}
+}
+
+func (e *AccrualServiceError) Error() string {
+	return fmt.Sprintf("accrual service error: %s", e.Status)
+}
+
+type UnexpectedStatusError struct {
+	StatusCode int
+}
+
+func NewUnexpectedStatusError(code int) *UnexpectedStatusError {
+	return &UnexpectedStatusError{StatusCode: code}
+}
+
+func (e *UnexpectedStatusError) Error() string {
+	return fmt.Sprintf("unexpected status code from accrual service: %d", e.StatusCode)
+}
--- internal/accrual/http.go
+++ internal/accrual/http.go
@@ -2,8 +2,8 @@ package accrual
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/go-resty/resty/v2"
+	"net/http"
 	"net/url"
 	"strconv"
 	"sufirmart/internal/domain"
@@ -24,24 +24,27 @@ func NewHttpReader(baseURL string) *HttpReader {
 }
 
 func (r *HttpReader) Get(orderNumber string) (*domain.Accrual, error) {
-	endpoint := fmt.Sprintf("%s/api/orders/%s", r.baseURL, url.PathEscape(orderNumber))
+	endpoint, err := url.JoinPath(r.baseURL, "api", "orders", orderNumber)
+	if err != nil {
+		return nil, NewBuildURLError(err)
+	}
 
 	resp, err := r.client.R().
 		SetHeader("Accept", "application/json").
 		Get(endpoint)
 	if err != nil {
-		return nil, err
+		return nil, NewRequestError(err)
 	}
 
 	switch resp.StatusCode() {
-	case 200:
+	case http.StatusOK:
 		var dto struct {
 			Order   string   `json:"order"`
 			Status  string   `json:"status"`
 			Accrual *float64 `json:"accrual,omitempty"`
 		}
 		if err := json.Unmarshal(resp.Body(), &dto); err != nil {
-			return nil, err
+			return nil, NewDecodeError(err)
 		}
 
 		orderNum, err := domain.NewOrderNumber(dto.Order)
@@ -61,10 +64,10 @@ func (r *HttpReader) Get(orderNumber string) (*domain.Accrual, error) {
 
 		return domain.NewAccural(orderNum, status, value), nil
 
-	case 204:
+	case http.StatusNoContent:
 		return nil, NewErrNotFound(orderNumber)
 
-	case 429:
+	case http.StatusTooManyRequests:
 		retryAfterHeader := resp.Header().Get("Retry-After")
 		var dur time.Duration
 		if retryAfterHeader != "" {
@@ -75,9 +78,9 @@ func (r *HttpReader) Get(orderNumber string) (*domain.Accrual, error) {
 		return nil, NewTooManyRequestsError(dur)
 
 	default:
-		if resp.StatusCode() >= 500 {
-			return nil, fmt.Errorf("accrual service error: %s", resp.Status())
+		if resp.StatusCode() >= http.StatusInternalServerError {
+			return nil, NewAccrualServiceError(resp.StatusCode(), resp.Status())
 		}
-		return nil, fmt.Errorf("unexpected status code from accrual service: %d", resp.StatusCode())
+		return nil, NewUnexpectedStatusError(resp.StatusCode())
 	}
 }
